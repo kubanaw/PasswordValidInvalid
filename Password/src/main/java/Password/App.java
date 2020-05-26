9ef18e53d52164049d2e3edda40d3b4fc6d7f15c
kubanaw <nawrocki90@gmail.com>
2020-05-26 20:28:51 +0200

    FirstCommit

---
+++ Password/src/main/java/Password/App.java
@@ -0,0 +1,13 @@
+package Password;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Password/src/test/java/Password/steps/Test.java
@@ -0,0 +1,13 @@
+package Password.steps;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		System.out.println(System.getProperty("webdriver.chrome.driver", Paths.get(System.getProperty("user.dir")).toRealPath() +  "\\src\\test\\java\\Password\\resources\\chromedriver.exe"));
+	}
+}
